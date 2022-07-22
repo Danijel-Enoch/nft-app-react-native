45d8127e0820603acf7ee0e3d4abd5553f12f26a
Danijel-Enoch <enochdanijel@gmail.com>
2022-07-22 21:58:46 +0100

    init commit

---
+++ screens/Details.js
@@ -0,0 +1,12 @@
+import { Text } from 'react-native-web'
+
+const Details = () => {
+  return (
+      <Text>
+          "Details Page"
+      </Text>
+    
+  )
+}
+
+export default Details
---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+import { Text } from 'react-native'
+const Home = () => {
+  return (
+      <Text>
+           "Home Page"
+      </Text>
+   
+  )
+}
+
+export default Home
